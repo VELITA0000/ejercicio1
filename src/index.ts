8ae5b6eeb3e25b084cfb14fc86d1be77dabedcef
David Vela <dvela020@gmail.com>
2025-09-17 10:09:40 -0600

    ejemplos de rutas de archivos

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import database from '../database.json';
 import routes from './app/routes';
+import { ejemplo } from './app/ejemplo2/ejemplo'
+import { index } from './app/ejemplo1'
 
 const PORT = 3000;
 
 const app = express();
+
+
+console.log('Ejemplo 1 sin poner en la ruta el nombre del archivo index:', index);
+console.log('Ejemplo 2 poniendo la ruta con el nombre del archivo:', ejemplo);
 
 app.use(routes);
 
