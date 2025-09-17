e3af2231fe6f861f94a25df64813f0ae8d4b3567
David Vela <dvela020@gmail.com>
2025-09-17 09:52:00 -0600

    correccion ruta user

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,6 +6,6 @@ import userRoutes from './users/routes';
 const router = Router();
 
 router.use('/auth', authRoutes);
-router.use('/users', authRoutes);
+router.use('/users', userRoutes);
 
 export default router;
